59894cb501f63d83b390963468f8aff103e478d0
Jiang Sheng <jiangshengdev@outlook.com>
2025-11-16 14:57:36 +0800

    stylistic block-like

--- eslint.config.ts
+++ eslint.config.ts
@@ -24,6 +24,8 @@ export default defineConfig([
       '@stylistic/padding-line-between-statements': [
         'error',
         { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: '*', next: 'block-like' },
+        { blankLine: 'always', prev: 'block-like', next: '*' },
         { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
         {
           blankLine: 'any',

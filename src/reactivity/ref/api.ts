36ce0c3ecbcdc58ee5aa563f694c5e49743689c4
Jiang Sheng <jiangshengdev@outlook.com>
2025-11-16 19:45:42 +0800

    refactor

--- src/reactivity/ref/api.ts
+++ src/reactivity/ref/api.ts
@@ -1,6 +1,6 @@
 import { ObjectRefImpl, RefImpl } from './impl.ts'
-import type { Ref } from './types.ts'
-import { REF_FLAG, type RefMarker } from './types.ts'
+import type { Ref, RefMarker } from './types.ts'
+import { REF_FLAG } from './types.ts'
 
 export function ref<T>(value: T): Ref<T> {
   if (isRef(value)) {

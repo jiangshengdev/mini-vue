754fa96a9b3496ef2758595bbb63b9c803b04d04
Jiang Sheng <jiangshengdev@outlook.com>
2025-11-16 14:39:36 +0800

    stylistic var

--- eslint.config.ts
+++ eslint.config.ts
@@ -24,6 +24,12 @@ export default defineConfig([
       '@stylistic/padding-line-between-statements': [
         'error',
         { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
       ],
     },
   },

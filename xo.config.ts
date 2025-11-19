1bb6036f0d74783519937e08cbdc211e15ea0d09
Jiang Sheng <jiangshengdev@outlook.com>
2025-11-19 10:56:00 +0800

    xo

--- xo.config.ts
+++ xo.config.ts
@@ -3,4 +3,9 @@ export default [
     space: true,
     prettier: 'compat',
   },
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    },
+  },
 ] satisfies import('xo').FlatXoConfig

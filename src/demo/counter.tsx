795ff2c1adf584192f692b6b0bb896479afcb764
Jiang Sheng <jiangshengdev@outlook.com>
2025-12-01 20:55:32 +0800

    demo

--- src/demo/counter.tsx
+++ src/demo/counter.tsx
@@ -1,28 +1,15 @@
-import type { ComponentType, ElementRef } from '@/index.ts'
-import { effect, reactive } from '@/index.ts'
+import type { ComponentType } from '@/index.ts'
+import { reactive } from '@/index.ts'
+
+const state = reactive({ count: 0 })
 
 export const Counter: ComponentType = () => {
-  const state = reactive({ count: 0 })
-  let buttonElement: HTMLButtonElement | undefined
-
-  effect(() => {
-    const label = `count is ${state.count}`
-
-    if (buttonElement) {
-      buttonElement.textContent = label
-    }
-  })
-
-  const bindRef: ElementRef = (element) => {
-    buttonElement = element as HTMLButtonElement
-  }
-
   const increment = () => {
     state.count += 1
   }
 
   return (
-    <button type="button" ref={bindRef} onClick={increment}>
+    <button type="button" onClick={increment}>
       count is {state.count}
     </button>
   )

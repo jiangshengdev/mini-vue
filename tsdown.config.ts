f8a6efdcfaa87f1aeacbe65dd91c079857fca4e3
Jiang Sheng <jiangshengdev@outlook.com>
2025-12-05 19:03:16 +0800

    jsx-runtime

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   exports: true,
-  // ...config options
+  entry: {
+    index: 'src/index.ts',
+    ['jsx-dev-runtime']: 'src/jsx-dev-runtime.ts',
+    ['jsx-runtime']: 'src/jsx-runtime.ts',
+  },
 })

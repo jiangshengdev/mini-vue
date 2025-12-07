60a8ffa78fce3fa176bf9f09979fb72e87e6020d
Jiang Sheng <jiangshengdev@outlook.com>
2025-12-07 22:37:39 +0800

    test browser

---
+++ vitest.browser.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
+import { mergeConfig } from 'vite'
+import vitestConfig from './vitest.config'
+
+export default mergeConfig(
+  vitestConfig,
+  defineConfig({
+    test: {
+      browser: {
+        enabled: true,
+        provider: playwright(),
+        // https://vitest.dev/config/browser/playwright
+        instances: [{ browser: 'chromium' }],
+      },
+    },
+  }),
+)

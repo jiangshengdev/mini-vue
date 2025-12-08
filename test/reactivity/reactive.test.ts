53963350f94f3112e54e15cd2e9a3d7ef0694b0a
Jiang Sheng <jiangshengdev@outlook.com>
2025-12-08 14:04:53 +0800

    import

--- test/reactivity/reactive.test.ts
+++ test/reactivity/reactive.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { reactive } from '@/index.ts'
-import { isReactive } from '@/index.ts'
+import { isReactive, reactive } from '@/index.ts'
 import type { PlainObject } from '@/shared/index.ts'
 
 describe('reactive', () => {

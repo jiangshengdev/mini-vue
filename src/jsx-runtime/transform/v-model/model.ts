d57815e5a469b7b563a68a9f348b83f8bf3bcf9a
Jiang Sheng <jiangshengdev@outlook.com>
2025-12-22 10:40:16 +0800

    refactor(jsx-runtime): 统一 v-model 相关函数命名规范

--- src/jsx-runtime/transform/v-model/model.ts
+++ src/jsx-runtime/transform/v-model/model.ts
@@ -2,18 +2,18 @@ import { jsxModelBindingReadonlyTarget } from '@/messages/index.ts'
 import { isRef } from '@/reactivity/index.ts'
 import { __DEV__ } from '@/shared/index.ts'
 
-export function readModelValue(model: unknown): unknown {
-  return isRef(model) ? model.value : model
+export function readModelValue(modelBinding: unknown): unknown {
+  return isRef(modelBinding) ? modelBinding.value : modelBinding
 }
 
-export function setModelValue(model: unknown, value: unknown): void {
-  if (isRef(model)) {
-    ;(model as { value: unknown }).value = value
+export function setModelValue(modelBinding: unknown, value: unknown): void {
+  if (isRef(modelBinding)) {
+    ;(modelBinding as { value: unknown }).value = value
 
     return
   }
 
   if (__DEV__) {
-    console.warn(jsxModelBindingReadonlyTarget, model)
+    console.warn(jsxModelBindingReadonlyTarget, modelBinding)
   }
 }

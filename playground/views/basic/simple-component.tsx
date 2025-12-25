824bc15a94590294d63b9349f8d73c12daa33334
Jiang Sheng <jiangshengdev@outlook.com>
2025-12-25 22:25:54 +0800

    refactor(playground): 使用 state 替代 ref 管理 groceryList

--- playground/views/basic/simple-component.tsx
+++ playground/views/basic/simple-component.tsx
@@ -1,10 +1,10 @@
 import type { Todo } from './todo-item.tsx'
 import { TodoItem } from './todo-item.tsx'
 import type { SetupComponent } from '@/index.ts'
-import { ref } from '@/index.ts'
+import { state } from '@/index.ts'
 
 export const SimpleComponent: SetupComponent = () => {
-  const groceryList = ref<Todo[]>([
+  const groceryList = state<Todo[]>([
     { id: 0, text: '蔬菜' },
     { id: 1, text: '奶酪' },
     { id: 2, text: '人类应该吃的其他东西' },
@@ -15,7 +15,7 @@ export const SimpleComponent: SetupComponent = () => {
       <section class="card">
         <h2>简单组件</h2>
         <ol>
-          {groceryList.value.map((item) => {
+          {groceryList.get().map((item) => {
             return <TodoItem key={item.id} todo={item} />
           })}
         </ol>
